0b14b472c95cf6805e1c79ef16a7bbb4e293fcfa
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-02-26 05:55:34 -0800

    End of day

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpFundamentals.Math;
+using System;
 
 namespace CSharpFundamentals
 {
@@ -6,6 +7,22 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
+            /*int hour = 19;
+
+            if (hour > 0 && hour < 12)
+                Console.WriteLine("It is morning.");
+            else if (hour >= 12 && hour < 18)
+                Console.WriteLine("It is afternoon");
+            else
+                Console.WriteLine("It is evening.");*/
+
+            /*bool isGoldCustomer = true;
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+
+            Console.WriteLine(price);*/
+
+            var season = Season.Autumn;
         }
     }
 }
